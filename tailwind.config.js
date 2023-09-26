fc125d010feb923fd3f64c90f8bbc58d91b32e86
Yash Suhagiya <54718565+yashsuhagiya@users.noreply.github.com>
2023-09-26 10:36:54 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    fontFamily: {
-      sans: ["Inter"],
-    },
     extend: {
       colors: {
         'neon-green': '#ffc3a0',
