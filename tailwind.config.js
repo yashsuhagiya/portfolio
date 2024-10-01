b3b130abf9f896c4daaab3cbc3301430d5167274
Yash Suhagiya <54718565+yashsuhagiya@users.noreply.github.com>
2024-10-01 14:17:37 +0000

    chore: Update font and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,23 @@
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      display: ['"MyfontYash"', "Arial", "cursive"],
+    },
     extend: {
       colors: {
-        'neon-green': '#48cfc4',
-        'primary': '#5471b6',
-        'secondary': '#48cfc4',
+        "neon-green": "#48cfc4",
+        primary: "#5471b6",
+        secondary: "#48cfc4",
       },
       backgroundImage: {
-        'blue-green-gradient': 'linear-gradient(247.23deg, #48cfc4 0%, #48cfc4 100%)',
+        "blue-green-gradient":
+          "linear-gradient(247.23deg, #48cfc4 0%, #48cfc4 100%)",
       },
     },
   },
   plugins: [],
   future: {
     hoverOnlyWhenSupported: true,
-  }
-}
+  },
+};
